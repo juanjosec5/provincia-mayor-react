ccb03622da395567f78ad742b67a6d43b4f2f3ad
Cuadros <juan.josecuadros1@gmail.com>
2024-01-17 18:47:52 -0500

    fixed img mobile safari

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,4 +6,4 @@ export const debounce = (func, wait) => {
   }
 }
 
-export const isSafari = ()  =>  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export const isSafari = () => /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
