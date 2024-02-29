8d309ab017e42eb403c06cc62d669c5a657275ff
Cuadros <juan.josecuadros1@gmail.com>
2024-02-28 19:35:15 -0500

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
@@ -10,6 +11,7 @@ import "./sass/main.scss";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
